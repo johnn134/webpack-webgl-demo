ea833918a62a2811c81308d64b8743d33fd2fac6
johnn134 <johnn134@gmail.com>
2017-11-21 22:08:32 -0500

    add initbuffers function

--- src/js/utils/webgl-utils.js
+++ src/js/utils/webgl-utils.js
@@ -64,4 +64,27 @@ const loadShader = (gl, type, source) => {
 
 };
 
-export { initShaderProgram };
+const initBuffers = gl => {
+
+    const positionBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    const positions = [
+         1.0,  1.0,
+        -1.0,  1.0,
+         1.0, -1.0,
+        -1.0, -1.0,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER,
+                  new Float32Array(positions),
+                  gl.STATIC_DRAW);
+
+    return {
+        position: positionBuffer
+    };
+
+};
+
+export { initShaderProgram, initBuffers };
