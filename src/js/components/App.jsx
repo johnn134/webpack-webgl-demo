908b563f60897d567c4b2f3097c0e6b8d7883a79
johnn134 <johnn134@gmail.com>
2017-11-21 22:09:31 -0500

    setup render function

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {initShaderProgram} from '../utils/webgl-utils';
+import {vec4} from 'gl-matrix';
 
 import vSource from '../../shaders/vert.glsl';
 import fSource from '../../shaders/frag.glsl';
@@ -34,6 +35,42 @@ class App extends Component {
 
         const shaderProgram = initShaderProgram(gl, vSource, fSource);
 
+        const programInfo = {
+            program: shaderProgram,
+            attribLocations: {
+              vertexPosition: gl.getAttribLocation(shaderProgram, 'vPosition'),
+            },
+            uniformLocations: {
+              modelMatrix: gl.getUniformLocation(shaderProgram, 'model'),
+              viewMatrix: gl.getUniformLocation(shaderProgram, 'view'),
+              perspectiveMatrix: gl.getUniformLocation(shaderProgram, 'perspective')
+            },
+        };
+
+    }
+
+    drawScene (gl, programInfo, buffers) {
+
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        const fieldOfView = 45 * Math.PI / 180;
+        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        const zNear = 0.1;
+        const zFar = 100.0;
+        const projectionMatrix = mat4.create();
+
+        mat4.perspective(projectionMatrix,
+                         fieldOfView,
+                         aspect,
+                         zNear,
+                         zFar);
+
+        const modelViewMatrix = mat4.create();
     }
 
     componentDidMount () {
